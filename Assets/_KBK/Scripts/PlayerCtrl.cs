5624731c7f0f78e4b442d979f0f9d37ab9bcb693
orionx6x <orionx6x@gmail.com>
2020-05-29 15:31:42 +0900

    게임 스테이지 제작

---
+++ Assets/_KBK/Scripts/PlayerCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float h = 0f;
+    private float v = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("h = " + h.ToString());
+        Debug.Log("v = " + v.ToString());
+    }
+}
