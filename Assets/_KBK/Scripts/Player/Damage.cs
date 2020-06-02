038db19ad431ccab192903b0a77ee3765acd447f
orionx6x <orionx6x@gmail.com>
2020-06-02 16:47:11 +0900

    UI 제작 - 혈흔 효과 (데미지) - Main Scene 제작 - HP Bar UI 제작 - 탄창 UI 제작

--- Assets/_KBK/Scripts/Player/Damage.cs
+++ Assets/_KBK/Scripts/Player/Damage.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
@@ -10,14 +10,27 @@ public class Damage : MonoBehaviour
     //생명게이지
     private float initHp = 100f;
     public float currHp;
+    //BloodScreen 텍스쳐를 저장하기 위한 변수
+    public Image bloodScreen;
 
     //델리게이트 및 이벤트 선언
     public delegate void PlayerDieHandler();
     public static event PlayerDieHandler OnPlayerDie;
 
+    //HP Bar Image를 저장하기 위한 변수
+    public Image hpBar;
+
+    //생명 게이지의 처음 색상(녹색)
+    private readonly Color initColor = new Vector4(0, 1.0f, 0f, 1f);
+    private Color currColor;
+
     void Start()
     {
         currHp = initHp;
+
+        //생명 게이지의 초기 색상 설정
+        hpBar.color = initColor;
+        currColor = initColor;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,14 +39,34 @@ public class Damage : MonoBehaviour
         {
             Destroy(other.gameObject);
 
+            //혈흔 효과 표현할 코루틴 함수 호출
+            StartCoroutine(ShowBloodScreen());
             currHp -= 0.5f;
             Debug.Log("Player HP = " + currHp.ToString());
+
+            //생명 게이지의 색상 및 크기 변경 함수 호출
+            DisplayHpBar();
 
             if(currHp <= 0)
             {
                 PlayerDie();
             }
         }
+    }
+
+    private void DisplayHpBar()
+    {
+        if ((currHp / initHp) > 0.5f)
+        {
+            currColor.r = (1 - (currHp / initHp)) * 2f;
+        }
+        else // 생명수치가 0%일 때는 노란색에서 빨간색으로 변경
+            currColor.g = (currHp / initHp) * 2f;
+
+        //HPBar 색상 변경
+        hpBar.color = currColor;
+        //HPBar 크기 변경
+        hpBar.fillAmount = (currHp / initHp);
     }
 
     private void PlayerDie()
@@ -47,10 +80,14 @@ public class Damage : MonoBehaviour
         //    enemies[i].SendMessage("OnPlayerDie", SendMessageOptions.DontRequireReceiver);
         //}
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    IEnumerator ShowBloodScreen()
     {
-        
+        //BloodScreen 텍스처의 알파값 불규칙하게 변경
+        bloodScreen.color = new Color(1, 0, 0, Random.Range(0.2f, 0.3f));
+        yield return new WaitForSeconds(0.1f);
+        //BloodScreen 텍스처의 색상 모두 0으로 변경
+        bloodScreen.color = Color.clear;
+
     }
 }
--- Assets/_KBK/Scripts/Player/FireCtrl.cs
+++ Assets/_KBK/Scripts/Player/FireCtrl.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //총알 발사와 재장전 오디오 클립을 저장할 구조체
 [System.Serializable]
@@ -37,6 +39,21 @@ public class FireCtrl : MonoBehaviour
     //Shake 클래스 저장할 변수
     private Shake shake;
 
+    //탄창 이미지 UI
+    public Image magazineImg;
+    //남은 총알 수 Text UI
+    public Text magazineText;
+
+    //최대 총알 수
+    public int maxBullet = 10;
+    //남은 총알 수
+    public int remainingBullet = 10;
+
+    //재장전 시간
+    public float reloadTime = 2f;
+    //재장전 여부 판단할 변수
+    public bool isReloading = false;
+
     void Start()
     {
         muzzleFlash = firePos.GetComponentInChildren<ParticleSystem>();
@@ -48,9 +65,15 @@ public class FireCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(!isReloading && Input.GetMouseButtonDown(0))
         {
+            --remainingBullet;
             Fire();
+
+            if(remainingBullet == 0)
+            {
+                StartCoroutine(Reloading());
+            }
         }
     }
 
@@ -63,6 +86,32 @@ public class FireCtrl : MonoBehaviour
         muzzleFlash.Play();
         // 사운드 발생
         FireSfx();
+
+        //재장전 이미지의 fillAmount 속성값 지정
+        magazineImg.fillAmount = (float)remainingBullet / (float)maxBullet;
+        //남은 총알 수 갱신
+        UpdateBulletText();
+    }
+
+    IEnumerator Reloading()
+    {
+        isReloading = true;
+        _audio.PlayOneShot(playerSfx.reload[(int)currWeapon], 1f);
+
+        //재장전 오디오 길이 + 0.3초동안 대기
+        yield return new WaitForSeconds(playerSfx.reload[(int)currWeapon].length + 0.3f);
+
+        //각종 변숫값의 초기화
+        isReloading = false;
+        magazineImg.fillAmount = 1f;
+        remainingBullet = maxBullet;
+        //남은 총알 수 갱신
+        UpdateBulletText();
+    }
+
+    private void UpdateBulletText()
+    {
+        magazineText.text = string.Format("<color=#ff0000>{0}</color>/{1}", remainingBullet, maxBullet);
     }
 
     void FireSfx()
